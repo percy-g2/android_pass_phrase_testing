fb18d7f21a07f24b95aca36afd96af42d62c390e
percy-g2 <gahlotpercy@gmail.com>
2022-03-20 22:05:39 +0530

    initial color changes.

--- app/src/main/java/org/androdevlinux/test/ui/main/MainFragment.kt
+++ app/src/main/java/org/androdevlinux/test/ui/main/MainFragment.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 import org.androdevlinux.test.R
 import org.androdevlinux.test.databinding.MainFragmentBinding
 
@@ -31,8 +33,12 @@ class MainFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
 
+        val customAdapter = CustomAdapter()
+        binding.passPhraseRecyclerView.apply {
+            adapter = customAdapter
+        }
         viewModel.randomListString.observe(viewLifecycleOwner) {
-            binding.value.text = it.toString()
+            customAdapter.updateList(it)
         }
 
         viewModel.generateRandom(12)
@@ -40,5 +46,46 @@ class MainFragment : Fragment() {
         binding.refreshBtn.setOnClickListener {
             viewModel.generateRandom(12)
         }
+    }
+}
+
+class CustomAdapter : RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
+
+    private var passPhraseList = listOf<MainViewModel.PassPhrase>()
+    // create new views
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        // inflates the card_view_design view
+        // that is used to hold list item
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.pass_phrase_item, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    fun updateList(mList: List<MainViewModel.PassPhrase>) {
+        passPhraseList = mList
+        notifyDataSetChanged()
+    }
+
+    // binds the list items to a view
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+        val ItemsViewModel = passPhraseList[position]
+        // sets the text to the textview from our itemHolder class
+        holder.textView.apply {
+            text = ItemsViewModel.passPhrase
+            setTextColor(context.resources.getColor(ItemsViewModel.passPhraseColor))
+        }
+
+    }
+
+    // return the number of the items in the list
+    override fun getItemCount(): Int {
+        return passPhraseList.size
+    }
+
+    // Holds the views for adding it to image and text
+    class ViewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView) {
+        val textView: TextView = itemView.findViewById(R.id.tv_phrase)
     }
 }
--- app/src/main/java/org/androdevlinux/test/ui/main/MainViewModel.kt
+++ app/src/main/java/org/androdevlinux/test/ui/main/MainViewModel.kt
@@ -5,10 +5,11 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import org.androdevlinux.test.R
 
 class MainViewModel : ViewModel() {
 
-    private val randomListStringInternal = MutableLiveData<String>()
+    private val randomListStringInternal = MutableLiveData<List<PassPhrase>>()
     val randomListString = randomListStringInternal
 
     private fun getNum(v: ArrayList<Int>): Int {
@@ -23,26 +24,36 @@ class MainViewModel : ViewModel() {
     fun generateRandom(n: Int) {
         viewModelScope.launch(Dispatchers.IO) {
             val v: ArrayList<Int> = ArrayList(n)
-            val randomList = mutableListOf<String>()
+            val randomList = mutableListOf<PassPhrase>()
 
             for (i in 0 until n) v.add(i.plus(1))
             while (v.size > 0) {
-                randomList.add(listOfPhase[getNum(v) - 1] + "\n")
+                val position = getNum(v) - 1
+                val test = listOfPhase.map { it.key }[position]
+                val value = listOfPhase.map { it.value }[position]
+                randomList.add(PassPhrase(test, value))
             }
-            randomListString.postValue(randomList.joinToString().replace(",", ""))
+            randomListString.postValue(randomList)
         }
     }
 
-    private val listOfPhase = listOf("impact",
-            "safe",
-            "include",
-            "life",
-            "health",
-            "nature",
-            "peace",
-            "spirit",
-            "improve",
-            "equal",
-            "ethics",
-            "evolve")
+    data class PassPhrase(
+        val passPhrase: String,
+        val passPhraseColor: Int
+    )
+
+    private val listOfPhase = hashMapOf(
+        "impact" to R.color.yellow,
+        "safe" to R.color.black,
+        "include" to R.color.light_brown,
+        "life" to R.color.red,
+        "health" to R.color.orange,
+        "nature" to R.color.light_green,
+        "peace" to R.color.light_blue,
+        "spirit" to R.color.cyan,
+        "improve" to R.color.pink,
+        "equal" to R.color.dark_blue,
+        "ethics" to R.color.indigio,
+        "evolve" to R.color.turquoise
+    )
 }
